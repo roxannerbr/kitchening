8249b3f253bde67fad0aca083736ddac6037b5c0
roxannerbr <romaroxana876@gmail.com>
2022-07-04 15:58:12 -0300

    cambios en main

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const livereload = require("livereload");
-const liveReloadServer = livereload.createServer();
+/* const livereload = require("livereload");
+const liveReloadServer = livereload.createServer(); */
 
 
 const express= require('express');
-const connectLivereload = require("connect-livereload");
+/* const connectLivereload = require("connect-livereload"); */
 const path= require('path');
 const app= express();
 const port= 3010;
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-liveReloadServer.watch(path.join(__dirname, 'public'));
-app.use(connectLivereload());
+/* liveReloadServer.watch(path.join(__dirname, 'public'));
+app.use(connectLivereload()); */
 
 app.get('/', (req, res)=> res. sendFile(path.join(__dirname, 'views', 'index.html')));
 
 
-liveReloadServer.server.once("connection", () => {
+/* liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
       liveReloadServer.refresh("/");
     }, 100);
-  });
+  }); */
 
-app.listen(port, ()=> console.log(`Servidor levantado con exito en http://localhost: ${port}}`));
+app.listen(port, ()=> console.log(`Servidor levantado con exito en http://localhost: ${port}`));
